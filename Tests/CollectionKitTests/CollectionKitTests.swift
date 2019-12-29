292a33a2953f56e730b41ec7848cdf9efe154069
Martin Johannesson <mjohannesson@gmail.com>
2019-12-29 11:58:14 +0100

    Initial Commit

---
+++ Tests/CollectionKitTests/CollectionKitTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import CollectionKit
+
+final class CollectionKitTests: XCTestCase {
+    func testExample() {
+        XCTAssertEqual("This test", "will fail.")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
